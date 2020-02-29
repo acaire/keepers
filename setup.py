f173e98afbec3b55e433c7ce4b65071943e251b6
Ash Caire <ash.caire@gmail.com>
2020-02-29 15:50:46 +0800

    And now use the right name

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 setup(
-    name='finders',
+    name='keepers',
     description=
-    'Finders - Find resources to determine if you want to keep them',
-    py_modules=['finders'],
-    version='0.1.0',
+    'Keepers - Find resources to determine if you want to keep them',
+    py_modules=['keepers'],
+    version='0.1.2',
     license='MIT',
     install_requires=[
         'boto3',
     ],
-    download_url='https://github.com/acaire/keepers/archive/0.1.0.tar.gz',
+    download_url='https://github.com/acaire/keepers/archive/0.1.2.tar.gz',
     url='https://github.com/acaire/keepers',
     author='Ash Caire',
     author_email='ash.caire@gmail.com',
